b971e4c9d199be02f067f60b21a1a02c3dab30ea
J-KC-Man <justin_man@hotmail.co.uk>
2019-05-20 18:57:07 +0100

    Add headers to login request

--- app/src/main/java/com/example/minimoneybox/datasource/ApiService.kt
+++ app/src/main/java/com/example/minimoneybox/datasource/ApiService.kt
@@ -5,6 +5,7 @@ import com.example.minimoneybox.datasource.model.UserLoginServerResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
@@ -13,6 +14,12 @@ interface ApiService {
     * Methods declared on an interface represent a single remote API endpoint.
     * Annotations describe how the method maps to an HTTP request.
     * */
+    @Headers(
+        "AppId: 3a97b932a9d449c981b595",
+        "Content-Type: application/json",
+        "appVersion: 5.10.0",
+        "apiVersion: 3.0.0"
+    )
     @POST("/users/login")
     fun login(@Body userLogin: UserLogin) : Deferred<Response<UserLoginServerResponse>>
 }
