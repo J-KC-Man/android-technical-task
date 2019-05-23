f0b7f68a8a02cea82a85e76b3b9cfc158fc1e579
J-KC-Man <justin_man@hotmail.co.uk>
2019-05-23 18:34:47 +0100

    Add repository wrapper method

--- app/src/main/java/com/example/minimoneybox/repository/Repository.kt
+++ app/src/main/java/com/example/minimoneybox/repository/Repository.kt
@@ -3,14 +3,18 @@ package com.example.minimoneybox.repository
 import com.example.minimoneybox.datasource.RemoteDataSource
 import com.example.minimoneybox.datasource.Result
 import com.example.minimoneybox.datasource.model.investorProducts.AllInvestorProductData
+import com.example.minimoneybox.datasource.model.payment.PaymentResponse
 
 class Repository(private val remoteDataSource: RemoteDataSource) {
 
-    suspend fun login(email : String, password: String) : Result<String> {
-        return remoteDataSource.loginUser(email, password)
-    }
+    suspend fun login(email : String, password: String) : Result<String> =
+        remoteDataSource.loginUser(email, password)
 
-    suspend fun getInvestorProducts(bearerToken : String?) : Result<AllInvestorProductData> {
-        return remoteDataSource.getInvestorProductsSafeCall(bearerToken)
-    }
+
+    suspend fun getInvestorProducts(bearerToken : String?) : Result<AllInvestorProductData> =
+        remoteDataSource.getInvestorProductsSafeCall(bearerToken)
+
+
+    suspend fun addOneOffPayment(bearerToken : String?, amount : String, investorProductId : String) : Result<PaymentResponse> =
+        remoteDataSource.addOneOffPaymentSafeCall(bearerToken, amount, investorProductId)
 }
